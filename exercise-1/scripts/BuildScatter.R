d8bc0f10b2a3e3372f970cd2d4f57ee2dfd3d120
Kimberly Cabamalan <cabakim@uw.edu>
2017-11-14 13:59:26 -0800

    add buildscatter function

--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -1,3 +1,4 @@
+library(ggplot2)
 # Function to build a scatterplot
 
 # Write a function that returns a scatterplot, parameterizing the following variables:
@@ -8,3 +9,8 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+
+scatterplot <- function(dataframe, x, y, color, title = "Title", xaxis = "x axis", yaxis = "y axis") {
+  result <- ggplot(data = dataframe) + geom_point(mapping = aes(x = x, y = y))
+  return (result)
+}
